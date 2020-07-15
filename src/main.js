c11455e36351e292a433e949658ca8691bf51f93
yangrong <rong6147855@foxmail.com>
2020-07-15 12:46:31 +0800

    影视导航

---
+++ src/main.js
@@ -0,0 +1,75 @@
+const $siteList = $(".siteList");
+const $lastLi = $siteList.find("li.last");
+const x = localStorage.getItem("x");
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+  { logo: "q", url: "https://v.qq.com/" },
+  { logo: "i", url: "https://www.iqiyi.com/" },
+  { logo: "B", url: "https://www.bilibili.com" },
+  { logo: "m", url: "https://www.mgtv.com/" },
+  { logo: "T", url: "http://tv.le.com/" },
+  { logo: "H", url: "https://www.hjtv5.com/" },
+  { logo: "Y", url: "https://www.youku.com/" },
+  { logo: "Q", url: "https://www.73m.cc/" },
+];
+const simplifyUrl = (url) => {
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .replace(/\/.*/, ""); // 删除 / 开头的内容
+};
+
+const render = () => {
+  $siteList.find("li:not(.last)").remove();
+  hashMap.forEach((node, index) => {
+    const $li = $(`<li>
+      <div class="site">
+        <div class="logo">${node.logo}</div>
+        <div class="link">${simplifyUrl(node.url)}</div>
+        <div class="close">
+          <svg class="icon">
+            <use xlink:href="#icon-close"></use>
+          </svg>
+        </div>
+      </div>
+    </li>`).insertBefore($lastLi);
+    $li.on("click", () => {
+      window.open(node.url);
+    });
+    $li.on("click", ".close", (e) => {
+      e.stopPropagation(); // 阻止冒泡
+      hashMap.splice(index, 1);
+      render();
+    });
+  });
+};
+
+render();
+
+$(".addButton").on("click", () => {
+  let url = window.prompt("请问你要添加的网址是啥？");
+  if (url.indexOf("http") !== 0) {
+    url = "https://" + url;
+  }
+  console.log(url);
+  hashMap.push({
+    logo: simplifyUrl(url)[0].toUpperCase(),
+    url: url,
+  });
+  render();
+});
+
+window.onbeforeunload = () => {
+  const string = JSON.stringify(hashMap);
+  localStorage.setItem("x", string);
+};
+
+$(document).on("keypress", (e) => {
+  const { key } = e;
+  for (let i = 0; i < hashMap.length; i++) {
+    if (hashMap[i].logo.toLowerCase() === key) {
+      window.open(hashMap[i].url);
+    }
+  }
+});
